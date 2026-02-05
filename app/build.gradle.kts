7a43db3d83695bf899e96b4f68ce4b9ca345bf2f
Sadekur Rahman <sadekur.rahman.talukder@gmail.com>
2026-02-05 09:59:55 +0600

    room dependency added successfully

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.ksp)
 }
 
 android {
@@ -58,6 +59,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/englishtutor/data/LessonEntity.kt
+++ app/src/main/java/com/example/englishtutor/data/LessonEntity.kt
@@ -1,4 +1,12 @@
 package com.example.englishtutor.data
 
-class LessonEntity {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "lessons")
+data class LessonEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val description: String
+)
